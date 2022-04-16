fa3094b88b4dc84da5e21d97e25744252399b0ad
Kajanth <kajanthkumar@gmail.com>
2022-04-16 14:06:24 -0400

    added scripts to build client into server folder

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
@@ -10,6 +11,10 @@ app.use(cors({
 // requests come in, gets checked for json type, goes through express router
 // parse any incoming json
 app.use(express.json());
+// serve static files
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(planetsRouter);
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 module.exports = app;
