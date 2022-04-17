7321c6cd5222cc7a511bc5766035aa6bf3547c94
Kajanth <kajanthkumar@gmail.com>
2022-04-17 15:47:05 -0400

    Basic Jest Setup

---
+++ server/src/routes/launches/launces.test.js
@@ -0,0 +1,22 @@
+// Test Fixture
+describe('Test GET /launches', () => {
+    // defines each test case
+    // First argument is name of test, second arguement is callback with test functions
+    test('It should respond with 200 success', () => {
+        const response = 200;
+        // Assertion, we expect a value to be ...
+        expect(response).toBe(200);
+    });
+})
+
+describe('Test POST /launch', () => {
+    test('It should respond with 200 sucess', () => {
+
+    });
+    test('It should catch missing required properties', () => {
+
+    });
+    test('It should catch invalid dates', () => {
+
+    });
+})
